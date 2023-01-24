e7d080501d7b53ca2cd59cd4e9c76cb1a090aac8
Aleksandra Chervonets <chervonets.as@gmail.com>
2023-01-24 15:20:47 +0300

    add preloader & error components

--- src/componenets/Users/Users.jsx
+++ src/componenets/Users/Users.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import Button from '../Button/Button';
 import SingleUser from './SingleUser/SingleUser';
 import FollowingGroup from './FollowingGroup/FollowingGroup';
+import Preloader from '../Preloader/Preloader';
+import Error from '../Error/Error';
 
 import style from './Users.module.css';
 
@@ -11,6 +13,10 @@ const Users = () => {
   const [users, setUsers] = useState([]);
   const [following, setFollowing] = useState([]);
   const [pageCount, setPageCount] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [isShowMoreLoading, setIsShowMoreLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     fetch(`https://social-network.samuraijs.com/api/1.0/users?page=1&count=5`)
@@ -18,7 +24,11 @@ const Users = () => {
       .then((response) => {
         setUsers(response.items)
       })
-      .catch((error) => console.log(error))
+      .catch((error) => setError(error.message))
+      .finally(() => {
+        setIsLoading(false)
+        setIsDisabled(false)
+      })
   }, [])
 
   const toggleFollowButton = (id, photos, name, followed) => {
@@ -41,12 +51,21 @@ const Users = () => {
   }
 
   const showMoreHandler = () => {
+    setIsShowMoreLoading(true)
     setPageCount(pageCount + 1)
     fetch(`https://social-network.samuraijs.com/api/1.0/users?page=${pageCount + 1}&count=5`)
       .then(response => response.json())
       .then((response) => {
         setUsers([...users, ...response.items])
       })
+      .catch((error) => setError(error.message))
+      .finally(() => setIsShowMoreLoading(false))
+  }
+
+  if (error) {
+    return (
+      <Error />
+    )
   }
 
   return (
@@ -54,13 +73,19 @@ const Users = () => {
       <FollowingGroup following={following} />
       <div className={style.usersInner}>
         <h4 className={style.usersTitle}>Users</h4>
-        {users.map(({ id, name, status, followed, photos }) => {
-          return (
-            <SingleUser id={id} key={id} name={name} photos={photos} status={status} followed={followed} toggleFollowButton={toggleFollowButton} />
-          )
-        })}
+        {isLoading
+          ? <Preloader />
+          : (users.map(({ id, name, status, followed, photos }) => {
+            return (
+              <SingleUser id={id} key={id} name={name} photos={photos} status={status} followed={followed} toggleFollowButton={toggleFollowButton} />
+            )
+          }))}
         <div className={style.showMore}>
-          <Button className={style.showMoreButton} text='Show more' onClick={showMoreHandler} />
+          {isShowMoreLoading
+            ? <Preloader />
+            : (isDisabled
+              ? null
+              : <Button className={style.showMoreButton} text='Show more' onClick={showMoreHandler} />)}
         </div>
       </div>
     </div>
